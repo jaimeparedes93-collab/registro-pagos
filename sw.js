fbf1ec58b608cf413ad457b68673b77b29860117
jaimeparedes93-collab <jaimeparedes93@gmail.com>
2026-06-17 21:35:27 -0500

    sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,34 @@
-self.addEventListener("install",e=>{
-  e.waitUntil(
-    caches.open("pagos-v1").then(c=>c.addAll(["./","index.html"]))
-  );
+const CACHE_NAME = "pagos-v2";
+
+self.addEventListener("install", event => {
+self.skipWaiting();
+
+event.waitUntil(
+caches.open(CACHE_NAME).then(cache => {
+return cache.addAll([
+"./",
+"./registro_pagos_pro.html"
+]);
+})
+);
+});
+
+self.addEventListener("activate", event => {
+event.waitUntil(
+caches.keys().then(keys =>
+Promise.all(
+keys
+.filter(key => key !== CACHE_NAME)
+.map(key => caches.delete(key))
+)
+)
+);
+
+self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+event.respondWith(
+fetch(event.request).catch(() => caches.match(event.request))
+);
 });
